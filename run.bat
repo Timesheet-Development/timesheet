e09e4ca40948e98a2a505d43b44a7c6600433774
sravan.nemala12 <sravan.nemala@ingwalabs.com>
2022-04-11 10:14:10 +0530

    Updated command database URL

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 set HTTP_PORT=8085
 set DEBUG_PRINTCONFIG=false
 set DEBUG_PRINTROOTCAUSE=true
-set COMMAND_DATABASE_URL=postgresql://postgres:1234@localhost:5432/postgres?sslmode=disable
+set COMMAND_DATABASE_URL=postgresql://doadmin:AVNS_mj2U3algtrX7hvC@db-postgresql-blr1-81158-do-user-11319451-0.b.db.ondigitalocean.com:25060/defaultdb?sslmode=require
 
 .\timesheet.exe
