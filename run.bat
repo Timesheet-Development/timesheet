6b5cf9d824219bc9f47b496a6cfd355ed296ff31
VenkataSaiSravan <sravannemala@gmail.com>
2022-04-12 22:03:32 +0530

    Updated db credentials

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 set HTTP_PORT=8085
 set DEBUG_PRINTCONFIG=false
 set DEBUG_PRINTROOTCAUSE=true
-set COMMAND_DATABASE_URL=postgresql://doadmin:AVNS_mj2U3algtrX7hvC@db-postgresql-blr1-81158-do-user-11319451-0.b.db.ondigitalocean.com:25060/defaultdb?sslmode=require
+set COMMAND_DATABASE_URL=postgresql://doadmin:AVNS_mj2U3algtrX7hvC@db-postgresql-blr1-81158-do-user-11319451-0.b.db.ondigitalocean.com:25061/pool1?sslmode=require
 
 .\timesheet.exe
